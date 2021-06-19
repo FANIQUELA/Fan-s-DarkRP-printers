5cabffdea908f728ad354775b59e219762abd180
FANIQUELA <44412752+FANIQUELA@users.noreply.github.com>
2021-06-19 09:28:15 +0300

    Update fani_config.lua

--- fan's_printers_v0.2.1/lua/fani_config.lua
+++ fan's_printers_v0.2.1/lua/fani_config.lua
@@ -1,47 +1,47 @@
 
-faniprintercurrency         = "$"  --Currency; ₺,$ etc. 
-faniratetranslate           = "Rate" --for translation (rate/speed)
-faniminutetranslate  		= "Min" --for translation (minute)
-fanimaterial	            = "models/debug/debugwhite"
-faniptypeselector           = "Tier" 
+faniprintercurrency                      = "$"  --Currency; ₺,$ etc. 
+faniratetranslate        	         = "Rate" --for translation (rate/speed)
+faniminutetranslate  			 = "Min" --for translation (minute)
+fanimaterial	       			 = "models/debug/debugwhite"
+faniptypeselector                        = "Tier" 
 							
-fanigreenname				= " 1 Printer" --edit this for change printers' name
-fanigreencolor				= Color( 20, 110, 60, 255 )
-fanigreenprintamount		= 50 --edit this for change rate (Default = 10 dollar every 5 secs) 
-fanigreenprinttime			= 1
+fanigreenname			         = " 1 Printer" --edit this for change printers' name
+fanigreencolor				 = Color( 20, 110, 60, 255 )
+fanigreenprintamount		         = 50 --edit this for change rate (Default = 10 dollar every 5 secs) 
+fanigreenprinttime		      	 = 1
 
-fanibluename				= " 2 Printer"
-fanibluecolor				= Color( 0, 0, 114, 255 )
-faniblueprintamount		    = 60	--edit this for change rate
-faniblueprinttime			= 1
+fanibluename				 = " 2 Printer"
+fanibluecolor				 = Color( 0, 0, 114, 255 )
+faniblueprintamount		         = 60	--edit this for change rate
+faniblueprinttime			 = 1
 
 
 
-faniyellowname				= " 3 Printer"
-faniyellowcolor		        = Color( 160, 160, 0, 255 )
-faniyellowprintamount		= 80 --edit this for change rate
-faniyellowprinttime		    = 1
+faniyellowname			   	 = " 3 Printer"
+faniyellowcolor		                 = Color( 160, 160, 0, 255 )
+faniyellowprintamount		         = 80 --edit this for change rate
+faniyellowprinttime		         = 1
 
 
 
 faniorangename				= " 4 Printer"
-faniorangecolor			    = Color( 174, 72, 0, 255 )
-faniorangeprintamount		= 100 --edit this for change rate
-faniorangeprinttime		    = 1
+faniorangecolor			        = Color( 174, 72, 0, 255 )
+faniorangeprintamount		        = 100 --edit this for change rate
+faniorangeprinttime		        = 1
 
 
 
-faniredname				    = " 5 Printer"
-faniredcolor				= Color( 154, 0, 0, 255 )
-faniredprintamount		    = 300 --edit this for change rate
-faniredprinttime			= 1
+faniredname				 = " 5 Printer"
+faniredcolor				 = Color( 154, 0, 0, 255 )
+faniredprintamount		         = 300 --edit this for change rate
+faniredprinttime			 = 1
 
 
 
-fanigodname				    = "God Tier Printer"
-fanigodcolor				= Color(85,34,63, 255 )
-fanigodprintamount		    = 1200 --edit this for change rate
-fanigodprinttime			= 1
+fanigodname				 = "God Tier Printer"
+fanigodcolor				 = Color(85,34,63, 255 )
+fanigodprintamount		         = 1200 --edit this for change rate
+fanigodprinttime			 = 1
 
 
   
