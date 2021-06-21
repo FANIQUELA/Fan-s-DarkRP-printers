86c0066b16ed95b3950bfd96b631385c42dbb6a3
FANIQUELA <44412752+FANIQUELA@users.noreply.github.com>
2021-06-21 20:21:31 +0300

    Update cl_init.lua

--- fan's_printers_v0.2.1/lua/entities/level1printer/cl_init.lua
+++ fan's_printers_v0.2.1/lua/entities/level1printer/cl_init.lua
@@ -10,17 +10,7 @@ surface.CreateFont( "PrinterFont", {
 	extended = false,
 	size = 75,
 	weight = 750,
-	blursize = 0,
-	scanlines = 0,
-	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
+	
 } )
 
 surface.CreateFont( "MoneyFont", {
@@ -28,34 +18,14 @@ surface.CreateFont( "MoneyFont", {
 	extended = false,
 	size = 60,
 	weight = 600,
-	blursize = 0,
-	scanlines = 0,
-	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
+
 } )
 surface.CreateFont( "uiFont", {
 	font = "Helvatica", --  Use the font-name which is shown to you by your operating system Font Viewer, not the file name
 	extended = false,
 	size = 30,
 	weight = 600,
-	blursize = 0,
-	scanlines = 0,
-	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
+
 } )
 
 
